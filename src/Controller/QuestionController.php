395c22e45147c8bf7bdc1af73bf2f32d955f7cb9
Evaldas <evabur@icloud.com>
2021-02-11 21:30:35 +0200

    added templates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,18 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
+    public function __construct(
+        private Environment $twig
+    ) {}
+
     #[Route('/', name: 'questions.homepage')]
     public function homepage(): Response
     {
-        return new Response('hello');
+        return new Response($this->twig->render('landing/index.html.twig'));
     }
 
     #[Route('/questions/{slug}', name: 'questions.show')]
     public function show($slug)
     {
-        return new Response($slug);
+        return new Response($this->twig->render('questions/show.html.twig', [
+            'question' => $slug 
+        ]));
     }
 }
