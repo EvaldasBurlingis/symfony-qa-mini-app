aa672b524b5fe6df2355af9fa2f97f3a9cf4c2bb
Evaldas <evabur@icloud.com>
2021-02-13 20:56:42 +0200

    refactored show question view

--- src/Form/AnswerFormType.php
+++ src/Form/AnswerFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Answer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,10 +15,15 @@ class AnswerFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', TextType::class, [
-                'label' => 'Your answer'
+            ->add('content', TextareaType::class, [
+                'label' => 'Your answer',
+                'label_attr' => ['class' => 'block text-xs text-blue-700 mb-1'],
+                'attr' => ['class' => 'border border-2 rounded-sm border-blue-200 p-2 w-full'],
+                'row_attr' => ['class' => 'mb-2']
             ])
-            ->add('Save', SubmitType::class);
+            ->add('Save', SubmitType::class, [
+                'attr' => ['class' => 'px-3 py-1 mr-2 bg-blue-600 text-blue-100 text-sm border border-blue-600 rounded-sm']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
